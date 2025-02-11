e81ec0296d6dc2199c346e32cf7e44f290b7dee3
pvlvRInc <97731849+pvlvRInc@users.noreply.github.com>
2025-02-11 14:00:07 +0500

    feat: tests project created

---
+++ src/TestEntityPostgre/ClientTest/ClientTest.cs
@@ -0,0 +1,45 @@
+﻿using FluentAssertions;
+using LicenseManagerClient.Extensions;
+using LicenseManagerClient.Services.Api.Base;
+
+namespace ClientTest;
+
+[TestFixture]
+public class Tests
+{
+    private ServerConnection? _serverConnection;
+    private ServerHttpClient? _httpClient;
+
+    [OneTimeSetUp]
+    public void Setup()
+    {
+        _httpClient ??= new ServerHttpClient("https://localhost:7133/");
+        _serverConnection ??= new ServerConnection(_httpClient, "LocalHost");
+        
+        _serverConnection.Connect();
+    }
+
+    [Test]
+    public async Task LoginTest()
+    {
+        
+        // Arrange
+        var testUsername = "pes@sutuli.com";
+        var testPassword = "!Ss123456";
+
+
+        // Act
+        var (result, response, resultData) = await _serverConnection.Login(testUsername, testPassword);
+
+        Console.WriteLine($"Result: {result} | Response message: {response.message.ToNiceJson()} | Token: {resultData.accessToken}");
+        
+        // Assert
+        _serverConnection.Token.Should().NotBeEmpty().And.NotBeNull();
+    }
+
+    [OneTimeTearDown]
+    public void Cleanup()
+    {
+        _httpClient.Dispose();
+    }
+}

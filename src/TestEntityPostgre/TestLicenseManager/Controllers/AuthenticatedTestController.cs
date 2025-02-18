9829137b3d7d94baa092cd57d06dc5a7738d9c93
pvlvRInc <97731849+pvlvRInc@users.noreply.github.com>
2025-02-18 09:22:56 +0500

    feat: log aggregator

--- src/TestEntityPostgre/TestLicenseManager/Controllers/AuthenticatedTestController.cs
+++ src/TestEntityPostgre/TestLicenseManager/Controllers/AuthenticatedTestController.cs
@@ -1,6 +1,7 @@
 using Library.Constants;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using Serilog;
 
 namespace TestLicenseManager.Controllers;
 
@@ -10,11 +11,17 @@ public class AuthenticatedTestController : ControllerBase
 {
     [Authorize]
     [HttpGet("get_login")]
-    public IActionResult GetName() => 
-        Ok($"Login: {User.Identity.Name}");
-    
+    public IActionResult GetName()
+    {
+        Log.Information("Getting name");
+        return Ok($"Login: {User.Identity.Name}");
+    }
+
     [Authorize(Roles = RoleConstants.Admin)]
     [HttpGet("get_role")]
-    public IActionResult GetAll() => 
-        Ok($"Login: {User.Identity.Name} Role: Admin");
+    public IActionResult GetAll()
+    {
+        Log.Information("Getting role");
+        return Ok($"Login: {User.Identity.Name} Role: Admin");
+    }
 }
--- src/TestEntityPostgre/TestLicenseManager/Program.cs
+++ src/TestEntityPostgre/TestLicenseManager/Program.cs
@@ -1,9 +1,27 @@
 using Microsoft.AspNetCore.Identity;
 using Microsoft.OpenApi.Models;
+using Serilog;
+using Serilog.Sinks.Grafana.Loki;
 using TestLicenseManager.Extensions;
 using TestLicenseManager.Models;
 
 var builder = WebApplication.CreateBuilder(args);
+
+Log.Logger = new LoggerConfiguration()
+            .MinimumLevel.Information()                                         // Минимальный уровень логирования
+            .WriteTo.GrafanaLoki("http://localhost:3100", labels: new List<LokiLabel>() { new(){Key = "job", Value = "api"}})
+            .WriteTo.Console()                                                  //Вывод логов в консоль
+            .WriteTo.File("logs/app.log", rollingInterval: RollingInterval.Day) // Запись в файл
+            .CreateLogger();
+
+// builder.Host.ConfigureLogging(logging =>
+// {
+//     logging.ClearProviders(); // Очищаем провайдеры логирования по умолчанию (например, Console)
+//     logging.AddSerilog();     // Добавляем Serilog
+//     // Можно добавить другие провайдеры логирования, если нужно
+//     // logging.AddDebug();
+// });
+
 
 // Add services to the container.
 builder.Services.AddControllers();
